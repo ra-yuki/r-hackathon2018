a779c0d46d1f27caebb0139e89295572aeb4b646
masaru.yamaguchi <ec2-user@ip-172-31-11-70.us-east-2.compute.internal>
2018-07-11 08:13:14 +0000

    edit group page

--- resources/views/group/makegroup.blade.php
+++ resources/views/group/makegroup.blade.php
@@ -4,10 +4,10 @@
 {!! Form::open(['route' => 'makegroup.store', 'method' => 'post']) !!}
       <dl>
         <dt>Group name</dt>
-        <dd><input type="text" name="name"></dd>
+        <dd><input type="text" name="name" placeholder="Group name"></dd> 
     </dl>
     @foreach($friends as $friend)
-        <input type="checkbox" name="friends[]" value={{$friend->id}}> {{$friend->name}} <br>
+        <input type="checkbox" name="friends[]" value={{$friend->id}}> {{$friend->name}} <br> 
     @endforeach
     <input type="submit" name="Create">
 {!! Form::close() !!}}
