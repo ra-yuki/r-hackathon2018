42c87c64571e2ccd3c08269ae605ece42f0fe5e2
masaru.yamaguchi <ec2-user@ip-172-31-11-70.us-east-2.compute.internal>
2018-07-13 00:09:27 +0000

    fixed navbar

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,23 +1,28 @@
-<nav class="navbar navbar-default navbar-static-top">
-	<div class="container-fluid">
-		<!--<div class="navbar-header">-->
-			<button type="button" class="navbar-toggle collapsed"data-toggle="collapse"data-target="#navbarEexample10">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
+<header>
+ @if (Auth::check())
+<nav class="navbar navbar-default">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+            </button>
+           	
+           	
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        	<ul class="nav navbar-defult">
+        	 <a  class="navbar-brand" href="/"> Medium rare </a>
+        	 <a class="navbar-left" href="/user" id="toop">Search </a>
+	
 			
-		<!--</div>-->
-		<a  class="navbar-brand" href="/"> Medium rare </a>
-		
-			<ul class="nav navbar-nav navbar-right">
 			    <div class="dropdown">
 			        <button class=" dropdown-toggle"  data-toggle="dropdown">
 		                Add plans
-		                <span class="caret"></span>
 	                </button>
-                	<ul class="dropdown-menu" role="menu">
+                	<ul class="dropdown-menu" >
                 		<a href="{{route('events.showSchedulePrivateEvent')}}" id="toop">+ Private Event </a>
                 		<a href="{{route('events.showScheduleGroupEvent')}}" id="toop">+ Group Event </a>
                 	</ul>
@@ -27,29 +32,33 @@
 			        <button class=" dropdown-toggle"  data-toggle="dropdown">
 		                Friends
 	                </button>
-                	<ul class="dropdown-menu" role="menu">
+                	<ul class="dropdown-menu" >
                 		<a class="navbar-left" href="/makegroup" id="toop">Makegroup </a>
-                		<a class="navbar-left" href="/friends" id="toop">Groups/Friends </a>
+                		<a class="navbar-left" href="/friends" id="toop">Show Friends/Groups </a>
                 	</ul>
 			    </div>
-			        
-			        <div class="dropdown">
+			    
+			    	<div class="dropdown">
 			        <button class=" dropdown-toggle"  data-toggle="dropdown">
 		                Settings
 	                </button>
-                	<ul class="dropdown-menu" role="menu">
+                	
+                	<ul class="dropdown-menu" >
                 		<a class="navbar-left" href="{{route('settings.settings')}}" id="toop">Settings</a>
                 		<p>Profile</p>
-                		@if (Auth::check())
-                                <a href="{{ route('logout.get') }}" class="bt"><div id="a">Log out</div></a>
-                            @else
-                            @endif
                         
-                	</ul>
+                    <a href="{{ route('logout.get') }}" class="bt"><div id="a">Log out</div></a>
+                    
+                    </ul>
 			    </div>
-				<a class="navbar-left" href="/user" id="toop">Search </a>
-			
+				
+		
 			</ul>
 		
+        
 	</div>
 </nav>
+@else
+                    
+@endif
+</header>
