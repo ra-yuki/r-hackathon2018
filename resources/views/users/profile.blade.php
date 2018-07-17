4651f79fdfec807aa6b6a62cfcbe8b48e6f4ce1f
EC2 Default User <ec2-user@ip-172-31-1-165.us-east-2.compute.internal>
2018-07-17 00:51:19 +0000

    fix

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,8 +2,8 @@
 
 
 @section('content')
-    <div class="row">
-        <aside class="col-xs-4">
+    <div class="row col-xs-4 col-xs-offset-4">
+        <aside class="col-xs-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                      <img class="media-object img-rounded img-responsive" src="moo.jpg" alt="icon"> 
