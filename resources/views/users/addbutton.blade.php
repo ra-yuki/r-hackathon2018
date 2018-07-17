63a1ac3bc26c3182c988715c6698460e30bee0fd
miyuokada <miyu.a.okada@rakuten.com>
2018-07-17 01:04:16 +0000

    button

--- resources/views/users/addbutton.blade.php
+++ resources/views/users/addbutton.blade.php
@@ -2,16 +2,16 @@
     @if (Auth::user()->is_friend($user->id))
 
     {!! Form::open(['route' => ['unfriend', $user->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfriend', ['class' => "btn btn-danger"]) !!}
-       
+        <!--{!! Form::submit('Unfriend', ['class' => "btn btn-danger"]) !!}-->
+       <button class="btn btn-default" type="submit"> <span class="glyphicon glyphicon-minus"></span> Unfriend</button>
         
     {!! Form::close() !!}
  
     @else
  
     {!! Form::open(['route' => ['add.get', $user->id], 'method' => 'get']) !!}
-        {!! Form::submit('Add', ['class' => "btn btn-success btn-xs"]) !!}
-        
+        <!--{!! Form::submit('Add', ['class' => "btn btn-success btn-xs"]) !!}-->
+        <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-plus"></span> Add</button>
      
     {!! Form::close() !!}
    
