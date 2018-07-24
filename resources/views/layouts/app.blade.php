e755b50772828ba8d563f576f592d4458d6e7e6e
meh <meh>
2018-07-24 05:42:45 +0000

    merging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
 
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>POPCON</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
