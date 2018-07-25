6d9c815f7e9bb37e64601a8397424079a1db62ca
EC2 Default User <ec2-user@ip-172-31-32-232.us-east-2.compute.internal>
2018-07-25 06:48:51 +0000

    intro

--- resources/views/users/introduction.blade.php
+++ resources/views/users/introduction.blade.php
@@ -14,13 +14,13 @@
 <div id="wrapper-top" class="container">
 <p class="yokoso">Welcome To POPCON</p>
 <h1>POPCONについて</h1>
-<p>POPCONは次世代のスケジューリングサービスである。<br>ユーザーの要望に合わせて、グループの中で最も参加人数の多い日程を自動算出することができる。</p><br><br>
+<p>POPCONは次世代のスケジューリングサービスである。</p><p style="margin-bottom:70px;">ユーザーの要望に合わせて、グループの中で最も参加人数の多い日程を自動算出することができる。</p>
  
  <h2>POPCONでできること</h2>
  <p>1.個人の予定を"Private Event"でたてることができる。</p>
-  <p>2.友達を追加し、グループを組むことができる。</p>
+ <p>2.友達を追加し、グループを組むことができる。</p>
  <p>3.グループの中でベストな日程がわかり、共通の予定をたてることができる。</p>
- <p>4.プロフィール画像やレイアウトを自分好みに変えることができる。</p>
+ <p style="margin-bottom:40px;">4.プロフィール画像やレイアウトを自分好みに変えることができる。</p>
  
  
  <h3>1.個人の予定をたてる</h3>
@@ -41,16 +41,25 @@
  
 <h3>3.グループの予定作成</h3>
  
- ナビバーの<img src="{{secure_asset('/images/add-user (1) (1).png')}}"  class="aaa"> "add plans" の "Group Event"、もしくは、Groupリストから予定を入力することができる。
+ ナビバーの<img src="{{secure_asset('/images/calendar.png')}}"  class="aaa"> "add plans" の "Group Event"、もしくは、Groupリストから予定を入力することができる。
 <br>そうすると、POPCONが自動的に各々の予定から都合のいい日を見つけ出す。<br>
-※Date fromとDate toの意味:自分とグループの皆が予定を組みたい期間を選択できる。
+※Date fromとDate toの意味:自分とグループの皆が予定を組みたい期間。
 
+<h3>4.プロフィール/レイアウト</h3>
+  <h4>プロフィール</h4>
+<p>ナビバーの"settings"にある"Profile"を選択し、"Choose File"で自分好みの画像に変更することができる。</p>
+ <h4>レイアウト</h4>
+ <p style="margin-bottom:40px;">ナビバーの"settings"にある"layout"からページ背景の色やテーマを変更することができる。</p>
+    
+    
 </div>
 
-<!--<h2>Mypageの見方</h2>-->
-<!--<p class="ime"><img src="{{secure_asset('/images/setumei.PNG')}}" class="aaa"></p>-->
+<h2>Mypageの見方</h2>
+<img src="{{secure_asset('/images/setumei.PNG')}}"class="ime">
 
-
-<a href="/mypage"><p class="sa">さっそく始めてみよう</p></a>
+<div class="row">
+    <a href="/mypage" class="btn" id="saa"><p class="sa">さっそく始めてみよう</p></a>
+    
+</div>
  </div>
 @endsection
